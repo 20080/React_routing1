c80716136c8a04ecac066c4a1e25328f94f4a0be
20080 <surajkumar2091@gmail.com>
2021-02-19 15:46:46 +0530

    Routing Tools

---
+++ src/Nav.js
@@ -0,0 +1,13 @@
+const Nav = ()=> {
+    return (
+        <nav>
+            <h3>Logo</h3>
+            <ul className={'nav-links'}>
+                <li>About</li>
+                <li>Shopping</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
