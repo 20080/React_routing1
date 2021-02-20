79e0f74b189b9c21570bed7d7d1078c4f3dafbc6
20080 <surajkumar2091@gmail.com>
2021-02-20 06:45:28 +0530

    Error M

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import './App.css';
 import Nav from "./Nav";
 import About from "./About";
 import Shop from "./Shop";
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
+      <Router>
     <div className="App">
       <Nav/>
-      <About/>
-      <Shop/>
+      <Route path={'/about'} component={About}/>
+      <Route path={'/shop'} component={Shop}/>
     </div>
+      </Router>
   );
 }
 
--- src/Nav.js
+++ src/Nav.js
@@ -1,10 +1,16 @@
+import {Link} from "react-router-dom";
+
 const Nav = ()=> {
     return (
         <nav>
             <h3>Logo</h3>
             <ul className={'nav-links'}>
+                <Link to='/about'>
                 <li>About</li>
+                </Link>
+                <Link to='/shop'>
                 <li>Shopping</li>
+                </Link>
             </ul>
         </nav>
     );
